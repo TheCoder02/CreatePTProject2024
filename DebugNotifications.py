da03d4ffed0723cbc1749879eece08641ac8daff
Lawson <97492153+TheCoder02@users.noreply.github.com>
2024-02-23 13:59:57 -0600

    Update DebugNotifications.py

--- DebugNotifications.py
+++ DebugNotifications.py
@@ -6,13 +6,10 @@ def test_function():
    logging.basicConfig(
        filename='debugLog.txt',
        level=logging.DEBUG,
-           filemode = 'w',
-       format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+       filemode = 'w',
+       format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
        datefmt='%Y-%m-%d %H:%M:%S',
-   )
-   logging.debug('This is a debug log')
-   
+      )
 
-
-ip()
-popup_maker()
+logging.debug(ip)
+popup_maker('You have Viruses!', 'file:///C:/Users/lg255111/OneDrive%20-%20Lamar%20Consolidated%20ISD/2023-2024/Computer%20Science/CreatePT%20Ideas/info.html', 'Click here to run diagnostics')
