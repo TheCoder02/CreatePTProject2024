b5de7cba5c912ff27d87f7100f382a8af43de4d0
Lawson <97492153+TheCoder02@users.noreply.github.com>
2024-03-20 14:04:54 -0500

    Update DebugNotifications.py

--- DebugNotifications.py
+++ DebugNotifications.py
@@ -1,15 +1,22 @@
-import logging
-import http.client
-from NotificationMaker import *
+#MAIN FILE, IMPORTS NOTIFICATION MAKER AND LINK OPEN IN ORDER TO MAKE POPUPS
+
 from LinkOpen import *
-def test_function():
-   logging.basicConfig(
-       filename='debugLog.txt',
-       level=logging.DEBUG,
-       filemode = 'w',
-       format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s',
-       datefmt='%Y-%m-%d %H:%M:%S',
+
+numbers = ['','','','','']
+for i in range(5):
+   number = int(input('Enter the 3 digit number to your lottery ticket to claim your prize!: '))
+   numbers[i] = number
+   
+x = random.randint(0,4)
+number = numbers[x]
+
+if number >= 100 and number >= 0:
+   print(number)
+   
+   popup_maker(
+      'You have Viruses!', 
+      'file:///C:/Users/lg255111/OneDrive%20-%20Lamar%20Consolidated%20ISD/2023-2024/Computer%20Science/CreatePT%20Ideas/info.html', 
+      'Click here to run diagnostics',
+      number
       )
 
-logging.debug(ip)
-popup_maker('You have Viruses!', 'file:///C:/Users/lg255111/OneDrive%20-%20Lamar%20Consolidated%20ISD/2023-2024/Computer%20Science/CreatePT%20Ideas/info.html', 'Click here to run diagnostics')
