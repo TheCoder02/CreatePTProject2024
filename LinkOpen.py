a285dd4870c646ce34fd2f8d55d51b5fd4d10162
Lawson <97492153+TheCoder02@users.noreply.github.com>
2024-03-20 14:05:11 -0500

    Update LinkOpen.py

--- LinkOpen.py
+++ LinkOpen.py
@@ -1,14 +1,43 @@
 from tkinter import *
-from time import *
+import time
+import random
 from NotificationMaker import *
 import webbrowser
-def popup_maker(title, link, text):
+def popup_maker(title, link, text, number):
    pop = Tk()
    pop.title(title)
-   def onClick():
-       webbrowser.open(link)
-
-   button = Button(pop,text=text,width = 50,height = 10,bg = 'white',fg = 'black',command=onClick)
+   def timedWindows(number):
+      for i in range(1):
+         for i in range(number):
+            x = random.randint(0,1820)
+            y = random.randint(0,980)
+            pop2 = Tk()
+            pop2.geometry('200x50'+'+'+str(x)+'+'+str(y))
+            pop2.title(title)
+            
+            timedWindow = Button(
+            pop2,
+            text = 'You got Hacked!',
+            bg = 'White',
+            fg = 'Black',
+            height = 10,
+            width = 50
+            )
+            
+            timedWindow.pack()      
+      pop.destroy()
+      pop2.mainloop()
+      
+   button = Button(
+   pop,
+   text = text,
+   width = 50,
+   height = 10,
+   bg = 'white',
+   fg = 'black',
+   command = timedWindows(number)
+   )
+   
    button.pack()
    pop.mainloop()
    return pop
